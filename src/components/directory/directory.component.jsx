a6c631b2eca3e4b82f4e4eaa415c63f04e749f4b
ThanishSid <64691478+ThanishSid@users.noreply.github.com>
2020-12-09 00:39:38 +0530

    updated menu-items to use react router through HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,7 +3,7 @@ import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
 const Directory = () => {
-  const [sections, setSections] = useState([
+  const [sections] = useState([
     {
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
@@ -40,8 +40,8 @@ const Directory = () => {
 
   return (
     <div className='directory-menu'>
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
   );
