b65d63e3e99dfac1c5b0bcd9ffbd0fac494035e7
Thanish Siddique <thanish.siddique@gmail.com>
2020-12-21 23:13:44 +0530

    implemented add to cart functionality

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+const CollectionItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const { name, price, imageUrl } = item;
+
+  return (
+    <div className='collection-item'>
+      <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => dispatch(addItem(item))} inverted>
+        Add to Cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
 export default CollectionItem;
