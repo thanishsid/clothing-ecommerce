50153cb9245efc1f513876e56ee8826cc484e0e9
thanishsid <thanish.siddique@gmail.com>
2020-12-24 23:47:57 +0530

    added functionality to prevent duplication of items in the cart and to show all items added to the cart in the drop down list

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
 
+import { useSelector } from 'react-redux';
+
+import CartItem from '../cart-item/cart-item.component';
+
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = () => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'></div>
-    <CustomButton>Go TO CHECKOUT</CustomButton>
-  </div>
-);
+const CartDropDown = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
+  return (
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>Go TO CHECKOUT</CustomButton>
+    </div>
+  );
+};
 
 export default CartDropDown;
