1d348f0ccd26c52deea1d0a7690b16242757769d
thanishsid <thanish.siddique@gmail.com>
2021-01-01 02:33:36 +0530

    implemented session persistence using redux persist in local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { store, persistor };
